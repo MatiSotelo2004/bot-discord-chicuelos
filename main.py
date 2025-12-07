769853092251fda3f4b21a4d15ec2f78fd5495e4
MatiSotelo2004 <matisotelo2004@gmail.com>
2025-12-07 16:27:15 -0300

    Implement keep_alive functionality and update channel ID

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from keep_alive import keep_alive
 import os
 
 # Configuración de Intents
@@ -9,7 +10,7 @@ intents.guilds = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
-ID_CANAL = 1446630362917896243
+ID_CANAL = 1443613756956479670
 
 @bot.event
 async def on_ready():
@@ -34,4 +35,5 @@ TOKEN = os.environ.get('DISCORD_TOKEN')
 if TOKEN is None:
     print("ERROR DE CONFIGURACIÓN: La variable de entorno 'DISCORD_TOKEN' no está configurada.")
 else:
+    keep_alive()
     bot.run(TOKEN)
